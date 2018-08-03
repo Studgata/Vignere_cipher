33cf2352d01075dfb8f14d2fdb6c616da06e8445
Studgata <34764380+Studgata@users.noreply.github.com>
2018-08-03 11:44:56 +0530

    Create vignere.py

---
+++ vignere.py
@@ -0,0 +1,50 @@
+count = 0
+ext_alph = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+alph_table = []
+for y in range(26):
+    new_row = []
+    for x in range(26):
+        new_row.append(ext_alph[x+count])
+    count += 1
+    alph_table.append(new_row)
+for x in alph_table:
+    print(x)
+
+
+def encrypt():
+    data = raw_input("Enter the data for cipher and Deciphering : ")
+    k = raw_input("Enter the key for the same : ")
+    data_length = len(data)
+    key_length = len(k)
+    nk = ""
+    kkkk=""
+    if data_length > key_length :
+        qout = data_length//key_length
+        
+        diff = data_length % key_length
+        d = k*qout  
+    for x in range(0,diff):
+        sliced = k[x]
+        kkkk = kkkk+sliced
+
+        
+    nk = nk + d+ kkkk
+    print(nk)
+    print(data_length)
+    print(len(nk))
+    row = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    column = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    encrypted_data = ""
+    if data_length == len(nk):    
+        for x in range(0,len(nk)+1):
+            fill= data[x]
+            keyto = nk[x]
+            c = row.find(fill)
+            r = column.find(keyto)
+            encr = alph_table[c][r]
+            encrypted_data = encrypted_data + encr
+        print(encrypted_data)
+            
+
+encrypt()
+        
